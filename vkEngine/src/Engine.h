d7ffe0b9780295e0d2397f62ae0d8c4ca9f6e20b
fl2mexd <kaidenchrisbird@gmail.com>
2023-05-06 15:32:34 +1000

    Fixed DLDI Issue, Added Physical Devices, Time to push to dev then master

--- vkEngine/src/Engine.h
+++ vkEngine/src/Engine.h
@@ -2,24 +2,31 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
+#include <set>
 
 class Engine
 {
 public:
-	Engine();
+	Engine();		
 	~Engine();
 private:
-	bool debugMode = true;
+	bool debugMode = false;
+
 	unsigned int screenWidth = 1600;
 	unsigned int screenHeight = 900;
+	GLFWwindow* m_Window{nullptr};
 
-	GLFWwindow* m_Window{ nullptr };
-
-	vk::Instance m_Instance{ nullptr };
-	vk::DebugUtilsMessengerEXT m_DebugMessenger{ nullptr };
-	vk::DispatchLoaderDynamic m_DLDI{ nullptr };
+	vk::Instance m_Instance{nullptr};
+	vk::DebugUtilsMessengerEXT m_DebugMessenger{nullptr};
+	vk::DispatchLoaderDynamic m_DLDI{};
+	vk::PhysicalDevice m_PhysicalDevice{nullptr};
 
 	void Initialize();
+
 	vk::Instance CreateInstance(bool debug, const char* applicationName);
+	
 	vk::DebugUtilsMessengerEXT CreateDebugMessenger(vk::Instance& instance, vk::DispatchLoaderDynamic& dldi);
+	
+	vk::PhysicalDevice CreateDevice(const vk::Instance& instance, const bool debug);
+
 };
